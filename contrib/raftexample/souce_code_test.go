cce4d747040c1822b31cacf82cf4bb8335244902
zhouzhi <zhouzhi@100tal.com>
2022-02-25 09:14:22 +0800

    记录写入prop流程日志

---
+++ contrib/raftexample/souce_code_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCommand(t *testing.T) {
+	//cd ~/Desktop/code/go_project/src/etcd/contrib/raftexample && go mod vendor && go build -o raftexample && ./raftexample --id 1 --cluster http://127.0.0.1:12379,http://127.0.0.1:22379,http://127.0.0.1:32379 --port 12380
+	//cd ~/Desktop/code/go_project/src/etcd/contrib/raftexample && ./raftexample --id 2 --cluster http://127.0.0.1:12379,http://127.0.0.1:22379,http://127.0.0.1:32379 --port 22380
+	//cd ~/Desktop/code/go_project/src/etcd/contrib/raftexample && ./raftexample --id 3 --cluster http://127.0.0.1:12379,http://127.0.0.1:22379,http://127.0.0.1:32379 --port 32380
+	//写入数据：curl -L http://127.0.0.1:22380/my-key12 -XPUT -d bar
+	//读取数据：curl -L http://127.0.0.1:32380/my-key
+	//添加节点：curl -L http://127.0.0.1:12380/4 -XPOST -d http://127.0.0.1:42379
+	//raftexample --id 4 --cluster http://127.0.0.1:12379,http://127.0.0.1:22379,http://127.0.0.1:32379,http://127.0.0.1:42379 --port 42380 --join
+	//删除节点：curl -L http://127.0.0.1:12380/3 -XDELETE
+}
+
+func TestGob(t *testing.T) {
+	var buf bytes.Buffer
+	gob.NewEncoder(&buf).Encode(map[string]interface{}{"a": "ss"})
+	fmt.Printf("%s\n", buf.String())
+
+	var data map[string]interface{}
+	dec := gob.NewDecoder(bytes.NewBufferString(buf.String()))
+	err := dec.Decode(&data)
+	fmt.Printf("%+v,%+v\n", data, err)
+}
+
+func TestTime(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	ticker := time.NewTicker(2000 * time.Millisecond)
+	go func(ticker *time.Ticker) {
+		<-ticker.C
+		t.Log("time out")
+		wg.Done()
+	}(ticker)
+
+	defer ticker.Stop()
+
+	go func(ticker *time.Ticker) {
+		<-ticker.C
+		t.Log("time out 2")
+		wg.Done()
+	}(ticker)
+
+	wg.Wait()
+}
+
+func TestCase(t *testing.T) {
+	type Demo struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+
+	c := make(chan Demo, 1)
+	a := Demo{
+		Name: "aa",
+		Age:  1,
+	}
+
+	select {
+	case c <- a: // 写不进去就抛弃
+		fmt.Print("aaa\n")
+	case <-c: // 读取数据
+	}
+
+	fmt.Printf("v:%+v", <-c)
+}
+
+func TestChan(t *testing.T) {
+	a := make(chan int, 3)
+	a <- 1 // 塞了一个数据
+	testChan(a)
+	c := <-a
+	c1 := <-a
+	c2 := <-a
+	fmt.Println(c, c1, c2)
+}
+
+func testChan(b chan<- int) {
+	b <- 2
+	b <- 3
+	close(b)
+}
