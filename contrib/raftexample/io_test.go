686acff1dac2798782f63437b1152a0fed2f0e48
zhouzhi <zhouzhi@100tal.com>
2022-03-08 19:56:10 +0800

    1. io包demo

--- contrib/raftexample/io_test.go
+++ contrib/raftexample/io_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -26,4 +27,50 @@ func TestMulti(t *testing.T) {
 
 	// Prints output
 	fmt.Printf("n:%v\n", Reader)
+}
+
+func TestIoReader(t *testing.T) {
+	reader1 := strings.NewReader("this is test")
+	b := make([]byte, 20)
+	//b := []byte{}  // 这样是不行的
+	n1, err := reader1.Read(b)
+	if err != nil {
+		fmt.Printf("err:%+v", err)
+	}
+
+	fmt.Printf("%+v\n", string(b[:n1]))
+
+	reader := strings.NewReader("Clear is better than clever")
+	p := make([]byte, 4)
+	for {
+		n, err := reader.Read(p)
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(string(p[:n]))
+	}
+}
+
+func TestWrite(t *testing.T) {
+	proverbs := []string{
+		"Channels orchestrate mutexes serialize",
+		"Cgo is not Go",
+		"Errors are values",
+		"Don't panic",
+	}
+	var writer bytes.Buffer
+
+	for _, p := range proverbs {
+		n, err := writer.Write([]byte(p))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if n != len(p) {
+			fmt.Println("failed to write data")
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println(writer.String())
 }
